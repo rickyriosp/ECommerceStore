7bb5d3e37e9fde779c2239b02124405ad13beba7
rickyriosp <riki.rios91@gmail.com>
2022-04-01 10:34:37 -0400

    shopping cart domain model

---
+++ ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,44 @@
+﻿namespace ShoppingCart.ShoppingCart
+{
+    public class ShoppingCart
+    {
+        private readonly HashSet<ShoppingCartItem> _items = new();
+
+        public int UserId { get; }
+        public IEnumerable<ShoppingCartItem> Items => _items;
+
+        public ShoppingCart(int userId)
+        {
+            this.UserId = userId;
+        }
+
+        public void AddItems(IEnumerable<ShoppingCartItem> shoppingCartItems)
+        {
+            foreach (var item in shoppingCartItems)
+            {
+                _items.Add(item);
+            }
+        }
+
+        public void RemoveItems(int[] productCatalogIds)
+        {
+            _items.RemoveWhere(i => productCatalogIds.Contains(i.ProductCatalogId));
+        }
+    }
+
+    public record ShoppingCartItem(int ProductCatalogId, string ProductName, string Description, Money Price)
+    {
+        public virtual bool Equals(ShoppingCartItem? obj)
+        {
+            var result = obj != null && this.ProductCatalogId.Equals(obj.ProductCatalogId);
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ProductCatalogId.GetHashCode();
+        }
+    }
+
+    public record Money(string Currency, decimal Amount);
+}
