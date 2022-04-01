0be8a9c6ea28cc8668dbd180d3df628318883768
rickyriosp <riki.rios91@gmail.com>
2022-04-01 17:45:33 -0400

    minimal event feed

---
+++ ShoppingCart/EventFeed/EventFeedController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingCart.EventFeed
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class EventFeedController : ControllerBase
+    {
+        private readonly IEventStore _eventStore;
+
+        public EventFeedController(IEventStore eventStore)
+        {
+            _eventStore = eventStore;
+        }
+
+        [HttpGet("")]
+        // Read the start and end values from a query string parameter
+        public Event[] Get([FromQuery] long start, [FromQuery] long end = long.MaxValue)
+        {
+            // Return the raw list of events. dotNET takes care
+            // of serializing the events into the response body
+            return _eventStore.GetEvents(start, end).ToArray();
+        }
+    }
+}
